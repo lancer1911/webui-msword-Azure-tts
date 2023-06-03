a6c3a411b73d38fcd965d9ed78f44a5cffc37ce0
lancer1911 <39460817+lancer1911@users.noreply.github.com>
2023-06-03 14:06:02 +0800

    Add files via upload

--- msword_azure_tts.py
+++ msword_azure_tts.py
@@ -3,6 +3,39 @@ import docx
 from pydub import AudioSegment
 import azure.cognitiveservices.speech as speechsdk
 from tqdm import tqdm
+from pdfminer.high_level import extract_text
+import chardet
+import docx2txt
+
+def read_docx(docx_file_path):
+    doc = docx.Document(docx_file_path)
+    full_text = []
+    for paragraph in doc.paragraphs:
+        full_text.append(paragraph.text)
+    return '\n'.join(full_text)
+
+def read_txt(file_path):
+    with open(file_path, 'rb') as f:
+        result = chardet.detect(f.read())
+        
+    with open(file_path, 'r', encoding=result['encoding']) as file:
+        text = file.read()
+    return text
+
+def read_pdf(pdf_file_path):
+    text = extract_text(pdf_file_path)
+    if text.strip() == "":
+        raise ValueError("The input PDF is image-based. Please upload a text-based PDF.")
+    return text
+
+def read_doc(doc_file_path):
+    try:
+        text = docx2txt.process(doc_file_path)
+        return text
+    except Exception as e:
+        print(e)
+        return None
+
 
 def read_docx(docx_file_path):
     doc = docx.Document(docx_file_path)
@@ -28,12 +61,23 @@ def text_to_speech(text, output_filename, subscription_key, region, voice_shortn
         if cancellation_details.reason == speechsdk.CancellationReason.Error:
             print(f"Error details: {cancellation_details.error_details}")
 
-def docx_to_mp3(docx_file_path, subscription_key, region, language, voice):
-    text = read_docx(docx_file_path)
+def docx_to_mp3(file_path, subscription_key, region, language, voice):
+    _, file_extension = os.path.splitext(file_path)
+    if file_extension == '.docx':
+        text = read_docx(file_path)
+    elif file_extension == '.txt':
+        text = read_txt(file_path)
+    elif file_extension == '.doc':
+        text = read_doc(file_path)
+    elif file_extension == '.pdf':
+        text = read_pdf(file_path)
+    else:
+        raise ValueError("Unsupported file type.")
+
     split_length = 2500 if language == "zh-CN" else 2000
     texts = [text[i:i + split_length] for i in range(0, len(text), split_length)]
 
-    input_filename_without_ext, _ = os.path.splitext(os.path.basename(docx_file_path))
+    input_filename_without_ext, _ = os.path.splitext(os.path.basename(file_path))
     output_filenames = []
     
     # Wrap your texts list with tqdm for a progress bar
@@ -58,3 +102,4 @@ def docx_to_mp3(docx_file_path, subscription_key, region, language, voice):
         
     print(f"All parts combined and saved to {combined_filename}")
     return combined_filename
+
